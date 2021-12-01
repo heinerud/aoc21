25f3448de63be6e42d354dc8bfc5492276bda4a2
Joel Heinerud <joel.heinerud@gmail.com>
2021-12-01 14:50:42 +0100

    Don't calculate sums for part 2

--- 01.py
+++ 01.py
@@ -1,16 +1,11 @@
-def increasing(l):
-    for a, b in zip(l[:-1], l[1:]):
+def increasing(l, d=1):
+    for a, b in zip(l[:-d], l[d:]):
         yield b > a
-
-
-def windows(l):
-    for a, b, c in zip(l[:-2], l[1:-1], l[2:]):
-        yield sum((a, b, c))
 
 
 if __name__ == "__main__":
     with open("01.in") as f:
         entries = [int(x) for x in f.readlines()]
 
-    print(sum(increasing(entries)))
-    print(sum(increasing(list(windows(entries)))))
+    print(sum(increasing(entries, 1)))
+    print(sum(increasing(entries, 3)))
