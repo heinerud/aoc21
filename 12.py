01a329aa8eaeae64228eb2e51264a2b7f632d5e4
Joel Heinerud <joel.heinerud@gmail.com>
2021-12-12 21:15:11 +0100

    Day 12: Queue instead of recursion

--- 12.py
+++ 12.py
@@ -3,58 +3,35 @@ import collections
 with open("12.in") as f:
     input = [x.strip().split("-") for x in f.readlines()]
 
-graph = collections.defaultdict(list)
+graph = collections.defaultdict(set)
 for a, b in input:
     if a == "end" or b == "start":
         a, b = b, a
 
-    graph[a].append(b)
+    graph[a].add(b)
 
     if a != "start" and b != "end":
-        graph[b].append(a)
+        graph[b].add(a)
 
 
-def search_1(graph, path, paths=None):
-    if paths is None:
-        paths = []
-    current = path[-1]
-    if current in graph:
+def search(graph, start, part2):
+    paths = 0
+    q = collections.deque([(start, set(), False)])
+    while q:
+        current, visited_small, twice = q.popleft()
+        if current == "end":
+            paths += 1
+            continue
         for child in graph[current]:
-            if child in path and child.islower():
-                continue
-            paths = search_1(graph, path + [child], paths)
-    elif current == "end":
-        paths += [path]
+            if child not in visited_small:
+                new_visited_small = set(visited_small)
+                if child.islower():
+                    new_visited_small.add(child)
+                q.append((child, new_visited_small, twice))
+            elif part2 and not twice:
+                q.append((child, visited_small, True))
     return paths
 
 
-def search_2(graph, path, double_visit, paths=None):
-    if paths is None:
-        paths = []
-    current = path[-1]
-    if current in graph:
-        for child in graph[current]:
-            if child != double_visit and child in path and child.islower():
-                continue
-            if child == double_visit and path.count(double_visit) > 1:
-                continue
-            paths = search_2(graph, path + [child], double_visit, paths)
-    elif current == "end":
-        paths += [path]
-    return paths
-
-
-# Part 1
-print(len(search_1(graph, ["start"])))
-
-
-# Part 2
-lowercase = set()
-for children in graph.values():
-    lowercase.update(children)
-lowercase.remove("end")
-
-paths = set()
-for x in lowercase:
-    paths.update(" ".join(x) for x in search_2(graph, ["start"], x))
-print(len(paths))
+print(search(graph, "start", False))
+print(search(graph, "start", True))
