a555b8cdc6f32d798ba3daa443025fbbeff7c4e2
Joel Heinerud <joel.heinerud@gmail.com>
2021-12-20 20:49:00 +0100

    Day 20: Add main function

--- 20.py
+++ 20.py
@@ -1,7 +1,7 @@
 import itertools
 
 
-def enhance(pixels, min_row, max_row, min_col, max_col):
+def enhance(pixels, alg, min_row, max_row, min_col, max_col):
     new_pixels = set()
     for r, c in itertools.product(
         range(min_row, max_row + 1), range(min_col, max_col + 1)
@@ -19,7 +19,7 @@ def enhance(pixels, min_row, max_row, min_col, max_col):
     return new_pixels
 
 
-def solve(pixels, iterations):
+def solve(pixels, alg, iterations):
     rows = set(x[0] for x in pixels)
     cols = set(x[1] for x in pixels)
     min_row = min(rows) - iterations * 2
@@ -28,25 +28,32 @@ def solve(pixels, iterations):
     max_col = max(cols) + iterations * 2
 
     for i in range(iterations):
-        pixels = enhance(pixels, min_row + i, max_row - i, min_col + i, max_col - i)
+        pixels = enhance(
+            pixels, alg, min_row + i, max_row - i, min_col + i, max_col - i
+        )
 
     return len(pixels)
 
 
-with open("./20.in") as f:
-    alg, img = f.read().split("\n\n")
+def main():
+    with open("./20.in") as f:
+        alg, img = f.read().split("\n\n")
 
-alg = [x == "#" for x in alg]
-pixels = set()
-for r, row in enumerate(img.splitlines()):
-    for c, x in enumerate(row):
-        if x == "#":
-            pixels.add((r, c))
+    alg = [x == "#" for x in alg]
+    pixels = set()
+    for r, row in enumerate(img.splitlines()):
+        for c, x in enumerate(row):
+            if x == "#":
+                pixels.add((r, c))
 
-p1 = solve(pixels, 2)
-print(p1)
-assert p1 == 5316
+    p1 = solve(pixels, alg, 2)
+    print(p1)
+    assert p1 == 5316
 
-p2 = solve(pixels, 50)
-print(p2)
-assert p2 == 16728
+    p2 = solve(pixels, alg, 50)
+    print(p2)
+    assert p2 == 16728
+
+
+if __name__ == "__main__":
+    main()
